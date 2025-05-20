4ce7f03f6d201a970d448557bf70bdf889d0a5f2
Randall Smith <Randall.Smith@amd.com>
2025-05-20 23:06:27 +0000

    add setup.sh
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+HOME=$1
+VIM_DIR=$HOME/.vim
+echo "Setting up in home directory:$HOME..."
+echo "Creating directories..."
+mkdir -p $VIM_DIR/autoload $VIM_DIR/backup
+echo "Download and install vim plug..."
+curl -LHO https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > $VIM_DIR/autoload/plug.vim
+echo "Download .vimrc..."
+curl -LHO https://raw.githubusercontent.com/rasmith/config/refs/heads/main/.vimrc  > $HOME/.vimrc
+echo "Download .tmux.conf..."
+curl -LHO https://raw.githubusercontent.com/rasmith/config/refs/heads/main/.tmux.conf  > $HOME/.tmux.conf
+echo "Install vim plug in vim..."
+vim +'PlugInstall --sync' +qa
+echo "Done."
